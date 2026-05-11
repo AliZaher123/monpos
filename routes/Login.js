f9601bcd80f023dc1681f131adf31f2acea0f3d8
Ali <azaher4499@gmail.com>
2026-05-11 20:01:24 +0100

    Session

--- routes/Login.js
+++ routes/Login.js
@@ -142,4 +142,41 @@ router.post("/", async (req, res) => {
 
 });
 
+
+router.post("/logout", (req, res) => {
+
+  if (!req.session) {
+    return res.status(400).json({ msg: "Pas de session" });
+  }
+
+  req.session.destroy((err) => {
+
+    if (err) {
+      console.log("LOGOUT ERROR:", err);
+      return res.status(500).json({ msg: "Erreur logout" });
+    }
+
+    res.clearCookie("connect.sid"); // supprime cookie session
+
+    console.log("SESSION DESTROYED");
+
+    return res.json({
+      msg: "Déconnecté avec succès"
+    });
+
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
